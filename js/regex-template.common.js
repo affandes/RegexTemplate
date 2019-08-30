4c3fbd7dab4c6a8a4396236a0d005fb4f5a10da2
Affandes <affandes@uin-suska.ac.id>
2019-08-30 17:29:36 +0700

    Update lagi

---
+++ js/regex-template.common.js
@@ -0,0 +1,1 @@
+module.exports = require('./regex-template.js');
--- js/regex-template.js
+++ js/regex-template.js
@@ -1,16 +1,13 @@
 /**
  * Regex Template v1.0.0
  */
-(function() {
-    'use strict';
+'use strict';
 
-    var obj = {
-        email: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g,
-        telp: /\w+/g
-    };
+var RegexTemplate = function () {
+    this.email = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g
+    this.telp = /\w+/g
+};
 
-    function coba() {
-        return obj;
-    }
+var obj = new RegexTemplate();
 
-}());
+module.exports = obj;
