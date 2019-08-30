0ca97912d912befba30fa6eae11a7176360516db
Affandes <affandes@uin-suska.ac.id>
2019-08-30 16:47:36 +0700

    Update lib

---
+++ js/regex-template.js
@@ -0,0 +1,16 @@
+/**
+ * Regex Template v1.0.0
+ */
+(function() {
+    'use strict';
+
+    var obj = {
+        email: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g,
+        telp: /\w+/g
+    };
+
+    function coba() {
+        return obj;
+    }
+
+}());
